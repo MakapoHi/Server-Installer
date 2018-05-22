07eb5998b48d2d863052a8272f56d550d7295b5b
Aleksey Makaronov <makapoh.lin@gmail.com>
2018-05-22 22:56:45 +0300

    sorry, this! :) so far, only steamcmd works

--- Server Installer/MainWindow.xaml.cs
+++ Server Installer/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using System.Diagnostics;
 
 namespace Server_Installer
 {
@@ -20,9 +24,59 @@ namespace Server_Installer
     /// </summary>
     public partial class MainWindow : Window
     {
+        enum VersionAmx { None, OneNineTwo, OneNineThree, OneNineThreeDev };
+
+        public int SelectedAmx { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton button = (RadioButton)sender;
+
+            if (button.Content.ToString() == "Amx Mod X 1.8.2")
+                SelectedAmx = (int)VersionAmx.OneNineTwo;
+            else if (button.Content.ToString() == "Amx Mod X 1.8.3")
+                SelectedAmx = (int)VersionAmx.OneNineThree;
+            else if (button.Content.ToString() == "Не устанавливать")
+                SelectedAmx = (int)VersionAmx.None;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string CurrentDir = Directory.GetCurrentDirectory();
+            string SteamCmdZipDir = String.Concat(CurrentDir, "\\steamcmd.zip");
+            string SteamCmdExeDir = String.Concat(CurrentDir, "\\steamcmd.exe");
+
+            if (!File.Exists(SteamCmdZipDir))
+            {
+                string SteamCmdUrl = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip";
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(SteamCmdUrl, "steamcmd.zip");
+            }
+            if (!File.Exists(SteamCmdExeDir))
+                ZipFile.ExtractToDirectory(SteamCmdZipDir, CurrentDir);
+
+            File.Delete(SteamCmdZipDir);
+
+            Start_SteamCmd(SteamCmdExeDir);
+        }
+
+        private void Start_SteamCmd(string SteamCmd_Dir = "")
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (process.ProcessName.Equals("steamcmd"))
+                {
+                    process.Kill();
+                    break;
+                }
+            }
+            Process.Start(SteamCmd_Dir);
         }
     }
 }
